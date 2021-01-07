457826f025d190d759604e7c517d0fcc8493cf85
YingBo.Dai <lyyb2001@163.com>
2021-01-07 22:08:49 +0800

    update：页面调整

--- blog-vue/blog/src/views/home/Home.vue
+++ blog-vue/blog/src/views/home/Home.vue
@@ -2,40 +2,6 @@
   <div>
     <!-- banner -->
     <div class="home-banner">
-      <div class="banner-container">
-        <!-- 联系方式 -->
-        <h1 class="blog-title animated zoomIn">
-          风丶宇的个人博客
-        </h1>
-        <!-- 一言 -->
-        <div class="blog-intro">
-          {{ obj.output }} <span class="typed-cursor">|</span>
-        </div>
-        <!-- 联系方式 -->
-        <div class="blog-contact">
-          <a
-            class="iconfont iconqq"
-            target="_blank"
-            href="http://wpa.qq.com/msgrd?v=3&uin=1192176811&site=qq&menu=yes"
-          />
-          <a
-            target="_blank"
-            href="https://github.com/X1192176811"
-            class="ml-5 mr-5 iconfont icongithub"
-          />
-          <a
-            target="_blank"
-            href="https://gitee.com/feng_meiyu"
-            class="iconfont icongitee-fill-round"
-          />
-        </div>
-      </div>
-      <!-- 向下滚动 -->
-      <div class="scroll-down" @click="scrollDown">
-        <v-icon color="#fff" class="scroll-down-effects">
-          mdi-chevron-down
-        </v-icon>
-      </div>
     </div>
     <!-- 主页文章 -->
     <v-row class="home-container">
@@ -206,10 +172,8 @@
 </template>
 
 <script>
-import EasyTyper from "easy-typer-js";
 export default {
   created() {
-    this.init();
     this.getBlogInfo();
     this.timer = setInterval(this.runTime, 1000);
   },
@@ -218,7 +182,6 @@ export default {
       tip: false,
       time: "",
       obj: {
-        output: "",
         isEnd: false,
         speed: 300,
         singleBack: false,
@@ -233,22 +196,6 @@ export default {
     };
   },
   methods: {
-    // 初始化
-    init() {
-      // 一言Api进行打字机循环输出效果
-      fetch("https://v1.hitokoto.cn?c=i")
-        .then(res => {
-          return res.json();
-        })
-        .then(({ hitokoto }) => {
-          this.initTyped(hitokoto);
-        });
-    },
-    initTyped(input, fn, hooks) {
-      const obj = this.obj;
-      // eslint-disable-next-line no-unused-vars
-      const typed = new EasyTyper(obj, input, fn, hooks);
-    },
     scrollDown() {
       window.scrollTo({
         behavior: "smooth",
@@ -257,7 +204,7 @@ export default {
     },
     runTime() {
       var timeold =
-        new Date().getTime() - new Date("December 12,2019").getTime();
+        new Date().getTime() - new Date("1/1/2021").getTime();
       var msPerDay = 24 * 60 * 60 * 1000;
       var daysold = Math.floor(timeold / msPerDay);
       var str = "";
@@ -305,33 +252,17 @@ export default {
 };
 </script>
 
-<style lang="stylus">
-.typed-cursor
-  opacity: 1
-  animation: blink 0.7s infinite
-@keyframes blink
-  0%
-    opacity: 1
-  50%
-    opacity: 0
-  100%
-    opacity: 1
-</style>
-
 <style scoped>
 .home-banner {
   position: absolute;
   top: -58px;
   left: 0;
   right: 0;
-  height: 100vh;
-  background: url("https://www.static.talkxj.com/ymwmek.jpg") center center /
-    cover no-repeat;
+  height: 60px;
   background-color: #49b1f5;
   background-attachment: fixed;
   text-align: center;
   color: #fff !important;
-  animation: header-effect 1s;
 }
 .banner-container {
   margin-top: 43vh;
@@ -370,7 +301,7 @@ export default {
   }
   .home-container {
     max-width: 1200px;
-    margin: calc(100vh - 50px) auto 0 auto;
+    margin: 0 auto 0 auto;
     padding: 0 3px;
   }
   .article-card {
@@ -430,15 +361,6 @@ export default {
     font-size: 1.25rem;
     transition: all 0.3s;
   }
-}
-.scroll-down {
-  cursor: pointer;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
-.scroll-down i {
-  font-size: 2rem;
 }
 .article-wrapper a:hover {
   color: #8e8cd8;
@@ -525,45 +447,5 @@ export default {
 .web-info {
   padding: 0.25rem;
   font-size: 0.875rem;
-}
-.scroll-down-effects {
-  color: #eee !important;
-  text-align: center;
-  text-shadow: 0.1rem 0.1rem 0.2rem rgba(0, 0, 0, 0.15);
-  line-height: 1.5;
-  display: inline-block;
-  text-rendering: auto;
-  -webkit-font-smoothing: antialiased;
-  animation: scroll-down-effect 1.5s infinite;
-}
-@keyframes scroll-down-effect {
-  0% {
-    top: 0;
-    opacity: 0.4;
-    filter: alpha(opacity=40);
-  }
-  50% {
-    top: -16px;
-    opacity: 1;
-    filter: none;
-  }
-  100% {
-    top: 0;
-    opacity: 0.4;
-    filter: alpha(opacity=40);
-  }
-}
-.big i {
-  color: #f00;
-  animation: big 0.8s linear infinite;
-}
-@keyframes big {
-  0%,
-  100% {
-    transform: scale(1);
-  }
-  50% {
-    transform: scale(1.2);
-  }
 }
 </style>
