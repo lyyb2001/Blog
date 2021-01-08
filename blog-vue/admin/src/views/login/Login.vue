8092411b601fe108d1eaf7c78e1d947b19a99824
YingBo.Dai <lyyb2001@163.com>
2021-01-08 21:59:28 +0800

    update：页面调整

--- blog-vue/admin/src/views/login/Login.vue
+++ blog-vue/admin/src/views/login/Login.vue
@@ -29,30 +29,12 @@
           placeholder="密码"
           @keyup.enter.native="handleLogin"
         >
-          <svg-icon
+          <el-icon
             slot="prefix"
             icon-class="password"
             class="el-input__icon input-icon"
           />
         </el-input>
-      </el-form-item>
-      <el-form-item prop="code">
-        <el-input
-          v-model="loginForm.code"
-          auto-complete="off"
-          placeholder="验证码"
-          style="width: 63%"
-          @keyup.enter.native="handleLogin"
-        >
-          <svg-icon
-            slot="prefix"
-            icon-class="validCode"
-            class="el-input__icon input-icon"
-          />
-        </el-input>
-        <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img" />
-        </div>
       </el-form-item>
       <el-checkbox
         v-model="loginForm.rememberMe"
@@ -81,8 +63,8 @@
 
 <script>
 // import { getCodeImg } from "@/api/login";
-// import Cookies from "js-cookie";
-// import { encrypt, decrypt } from "@/utils/jsencrypt";
+import Cookies from "js-cookie";
+import { encrypt, decrypt } from "@/utils/jsencrypt";
 
 export default {
   name: "Login",
@@ -111,16 +93,16 @@ export default {
     };
   },
   watch: {
-    // $route: {
-    //   handler: function(route) {
-    //     this.redirect = route.query && route.query.redirect;
-    //   },
-    //   immediate: true
-    // }
+    $route: {
+      handler: function(route) {
+        this.redirect = route.query && route.query.redirect;
+      },
+      immediate: true
+    }
   },
   created() {
     // this.getCode();
-    // this.getCookie();
+    this.getCookie();
   },
   methods: {
     // getCode() {
@@ -129,46 +111,44 @@ export default {
     //     this.loginForm.uuid = res.uuid;
     //   });
     // },
-    // getCookie() {
-    //   const username = Cookies.get("username");
-    //   const password = Cookies.get("password");
-    //   const rememberMe = Cookies.get("rememberMe");
-    //   this.loginForm = {
-    //     username: username === undefined ? this.loginForm.username : username,
-    //     password:
-    //       password === undefined ? this.loginForm.password : decrypt(password),
-    //     rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
-    //   };
-    // },
-    // handleLogin() {
-    //   this.$refs.loginForm.validate(valid => {
-    //     if (valid) {
-    //       this.loading = true;
-    //       if (this.loginForm.rememberMe) {
-    //         Cookies.set("username", this.loginForm.username, { expires: 30 });
-    //         Cookies.set("password", encrypt(this.loginForm.password), {
-    //           expires: 30
-    //         });
-    //         Cookies.set("rememberMe", this.loginForm.rememberMe, {
-    //           expires: 30
-    //         });
-    //       } else {
-    //         Cookies.remove("username");
-    //         Cookies.remove("password");
-    //         Cookies.remove("rememberMe");
-    //       }
-    //       this.$store
-    //         .dispatch("Login", this.loginForm)
-    //         .then(() => {
-    //           this.$router.push({ path: this.redirect || "/" }).catch(() => {});
-    //         })
-    //         .catch(() => {
-    //           this.loading = false;
-    //           this.getCode();
-    //         });
-    //     }
-    //   });
-    // }
+    getCookie() {
+      const username = Cookies.get("username");
+      const password = Cookies.get("password");
+      this.loginForm = {
+        username: username === undefined ? this.loginForm.username : username,
+        password:
+          password === undefined ? this.loginForm.password : decrypt(password),
+      };
+    },
+    handleLogin() {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.loading = true;
+          if (this.loginForm.rememberMe) {
+            Cookies.set("username", this.loginForm.username, { expires: 30 });
+            Cookies.set("password", encrypt(this.loginForm.password), {
+              expires: 30
+            });
+            Cookies.set("rememberMe", this.loginForm.rememberMe, {
+              expires: 30
+            });
+          } else {
+            Cookies.remove("username");
+            Cookies.remove("password");
+            Cookies.remove("rememberMe");
+          }
+          this.$store
+            .dispatch("Login", this.loginForm)
+            .then(() => {
+              // this.$router.push({ path: this.redirect || "/" }).catch(() => {});
+            })
+            .catch(() => {
+              this.loading = false;
+              // this.getCode();
+            });
+        }
+      });
+    }
   }
 };
 </script>
@@ -178,8 +158,8 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
-  /*background-image: url("../assets/images/login-background.jpg");*/
+  height: 100vh;
+  background-image: url("../../assets/images/login-background.jpg");
   background-size: cover;
 }
 .title {
